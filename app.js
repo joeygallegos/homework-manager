64257ace4adc0bd3221a64e498a26d5cfdc38b91
João Pedro <joni_silvaco@hotmail.com>
2014-10-09 09:34:02 +0100

    We can now add disciplines to a MongoDB database

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require("express")
+var monk = require("monk")
+
+var APIResponse = require("./lib/APIResponse")
+
 var app = express()
+var db = monk("localhost:27017/homework-manager")
 var server = app.listen(3000, function() {
   console.log("Server running on port %d", server.address().port)
 })
@@ -9,18 +14,51 @@ app.set("view engine", "ejs")
 app.set("view cache", true)
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.redirect("/list") //temporary redirect
 })
 
-app.get("/list", function (req, res) {
+app.get("/list", function(req, res) {
   res.render("list", {title: "Homework List"})
 })
 
-app.get("/manage", function (req, res) {
+app.get("/manage", function(req, res) {
   res.render("manage", {title: "Discipline Management"})
 })
 
-app.use(function (req, res, next) {
+app.get("/api/discipline/add", function(req, res) {
+  var response = new APIResponse()
+  var name = req.param("name")
+  var label = req.param("label")
+
+  var collection = db.get("disciplines");
+  /*var test = collection.find({$or: {label: label, name: name}}).count(function(e, count) {
+    console.log(e + ":" + count)
+  })*/
+
+  collection.find({$or: [{label: label}, {name: name}]}, function(err, doc) {
+    if (err) {
+      response.setResponse("error", err.message)
+      return response.sendResponse(res)
+    }
+
+    if (doc.length > 0) {
+      response.setResponse("error", "The label or the discipline already exists!")
+      response.sendResponse(res)
+    } else {
+      collection.insert({label: label, name: name}, function(err, doc) {
+        if (err) {
+          response.setResponse("error", err.message)
+          return response.sendResponse(res)
+        }
+
+        response.setResponse("success", "Discipline added!")
+        response.sendResponse(res)
+      })
+    }
+  })
+})
+
+app.use(function(req, res, next) {
   res.status(404).send("Error 404: File not found.")
 })
